912c46b387881f0a095274b1dc96eafb44971ab6
AMARENDER REDDY <AMARENDER REDDY>
2017-03-17 13:31:23 -0400

    first commit

---
+++ Assets/Scripts/Racket.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Racket : MonoBehaviour {
+
+    // Movement Speed
+    public float speed = 150;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        // Get Horizontal Input
+        float h = Input.GetAxisRaw("Horizontal");
+
+        // Set Velocity (movement direction * speed)
+        GetComponent<Rigidbody2D>().velocity = Vector2.right * h * speed;
+
+    }
+}
